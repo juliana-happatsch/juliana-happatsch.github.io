185857e7f3145da60f1cfa787f32af7702dd1131
Juliana Happatsch <jhappatsch@gmail.com>
2021-09-19 17:31:35 -0300

    completa página start (mobile)

--- src/pages/start.js
+++ src/pages/start.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import '../css/start.css';
 
 function Start() {
   return (
     <main id="start-body">
       <section className="glowing">
-        <span className="sparkle1" style={ { '--i': 3 } }></span>
-        <span className="sparkle2" style={ { '--i': 5 } }></span>
-        <span className="sparkle3" style={ { '--i': 8 } }></span>
-        <span className="sparkle4" style={ { '--i': .7 } }></span>
-        <span className="sparkle5" style={ { '--i': 9 } }></span>
+        <span className="sparkle1"></span>
+        <span className="sparkle2"></span>
+        <span className="sparkle3"></span>
+        <span className="sparkle4"></span>
+        <span className="sparkle5"></span>
+
+        <img src="../images/ticker.png" alt="ticket" />
       </section>
+      
+      <Link to="/home">
+        <button className="btn start-btn">Start</button>
+      </Link>
     </main>
   )
 }
