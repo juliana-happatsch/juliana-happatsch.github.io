d4abc08b3317634f6d2efa0c02adddfb3bcafa98
juliana-happatsch <jhappatsch@gmail.com>
2021-11-20 14:36:18 -0300

    ahco q foi

--- src/components/infoCard.js
+++ src/components/infoCard.js
@@ -2,26 +2,50 @@ import { React, useEffect, useState } from 'react';
 import '../css/infoCard.css';
 
 function InfoCard() {
-  const [techs, setTechs] = useState('HTML, CSS, JS, React +');
-  const [birthday, setBirthday] = useState('March 1st, 2001');
-  const [name, setName] = useState('Juliana Happatsch');
-  
+  const windowFirstSize = window.innerWidth;
+  let firstTechs = '';
+  let firstBirthday = '';
+  let firstName = '';
+      
+  if (windowFirstSize >= 421 && windowFirstSize > 420 && windowFirstSize < 670) {
+    firstTechs = 'HTML, CSS, JS, React +';
+    firstBirthday = 'March 1st, 2001';
+    firstName = 'Juliana Happatsch';
+  } else if (windowFirstSize <= 420 && windowFirstSize > 360) {
+    firstTechs = 'HTML, CSS, JS +';
+    firstBirthday = 'March 1st, 2001';
+    firstName = 'Juliana Happatsch';
+  } else if (windowFirstSize <= 360) {
+    firstTechs = 'HTML, CSS +';
+    firstBirthday = '01/03/2001';
+    firstName = 'Juliana';
+  } else if (windowFirstSize >= 670) {
+    firstTechs = 'HTML, CSS, JavaScript, React, Redux, Context API +';
+    firstBirthday = 'March 1st, 2001';
+    firstName = 'Juliana Happatsch';
+  }
+
+  const [techs, setTechs] = useState(firstTechs);
+  const [birthday, setBirthday] = useState(firstBirthday);
+  const [name, setName] = useState(firstName);
 
   useEffect(() => {
     const handleWindowSize = () => {
       const windowWidth = window.innerWidth;
-      if (windowWidth >= 670) setTechs('HTML, CSS, JavaScript, React, Redux, Context API +');
-      if (windowWidth <= 500 || windowWidth <= 1024 || windowWidth <= 1160) setTechs('HTML, CSS, JavaScript, React +');
-      if (windowWidth <= 380) setTechs('HTML, CSS, JS +');
+      
+      if (windowWidth <= 421) setTechs('HTML, CSS, JS, React +');
+      if (windowWidth <= 420) setTechs('HTML, CSS, JS +');
       if (windowWidth <= 360) {
         setTechs('HTML, CSS +');
         setBirthday('01/03/2001');
         setName('Juliana');
       }
+      if (windowWidth >= 670) setTechs('HTML, CSS, JavaScript, React, Redux, Context API +');
     };
 
+    window.addEventListener('load', handleWindowSize);
     window.addEventListener('resize', handleWindowSize);
-  }, [techs]);
+  }, [techs, windowFirstSize]);
 
   return (
     <div className='info-card'>
